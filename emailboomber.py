077558887ca43549349b3484e5078493008a2cc9
Monir Hasan <admin@monirimonetizeit.com>
2025-08-20 15:16:13 -0700

    First commit: added email sender script

---
+++ emailboomber.py
@@ -0,0 +1,16 @@
+ano 8.5                    emailboomber.py                     Modified
+        print(Fore.RED + "\n❌ Authentication failed. Use an App Password, not >
+    except KeyboardInterrupt:
+        print("\n[-] Canceled by user")
+        sys.exit()
+    except Exception as e:
+        print(Fore.RED + f"\n⚠️ Error: {e}")
+        sys.exit()
+
+if __name__ == "__main__":
+    show_banner()
+    send_test_email(
+
+
+
+
